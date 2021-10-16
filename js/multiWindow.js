065c9fae1f16112aae75b98cf8375b56b8ea783b
ed100miles <edmilestech@gmail.com>
2021-10-16 11:06:21 +0100

    sync changes for multimission, styling changes

--- js/multiWindow.js
+++ js/multiWindow.js
@@ -1,9 +1,6 @@
 const electron = require('electron');
-const { app } = require('electron')
 const path = require('path')
 const fs = require('fs');
-const { resolve } = require('path');
-const { rejects } = require('assert');
 const { ipcRenderer } = electron;
 const form = document.getElementById('form')
 const dl_links = document.getElementById('dl_links')
@@ -12,6 +9,7 @@ const configFile = path.join(__dirname, '../configs/pos_config.json')
 // Pass file path to main.js to be sent to pos.py
 form.addEventListener('submit', (e) => {
     e.preventDefault()
+    dl_links.style.display = 'none'
     const file = document.getElementById('upload_file').files[0].path + `,${configFile}`
     console.log(file)
     ipcRenderer.send('file', file)
@@ -22,33 +20,32 @@ ipcRenderer.on('files_done', (_, data) => {
     data = JSON.parse(data)
     let dl_good = path.join(data["dl_path"], 'good.txt')
     let dl_bad = path.join(data["dl_path"], 'bad.txt')
-    let deletedFiles = new Promise((resolve, reject) => {
-        fs.unlink(dl_good, (err) => {
+    try{
+        fs.unlinkSync(dl_good, (err) => {
             if (err) {
                 console.error(err)
                 return
             }
         })
-        fs.unlink(dl_bad, (err) => {
+        fs.unlinkSync(dl_bad, (err) => {
             if (err) {
                 console.error(err)
                 return
             }
         })
-        resolve('done')
-    })
-    deletedFiles.then((message) => {
-        for (const value of Object.values(data['good'])) {
-            fs.appendFileSync(dl_good, `${value}\n\n`, (err) => {
-                if (err) throw err;
-            })
-        }
-        for (const value of Object.values(data['bad'])) {
-            fs.appendFileSync(dl_bad, `${value}\n\n`, (err) => {
-                if (err) throw err;
-            })
-        }
-    })
+    } catch (error){
+        console.log(error)
+    }
+    for (let value of Object.values(data['good'])) {
+        fs.appendFileSync(dl_good, `${value}\n\n`, (err) => {
+            if (err) throw err;
+        })
+    }
+    for (let value of Object.values(data['bad'])) {
+        fs.appendFileSync(dl_bad, `${value}\n\n`, (err) => {
+            if (err) throw err;
+        })
+    }
     console.log('files are done...')
     dl_links.style.display = 'block'
 })
